327018cff101f35951e8b98a09f130d4c50341bf
Saloni Parate <paratesaloni@gmail.com>
2026-08-06 23:51:36 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,26 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
+
+// Load env variables
 dotenv.config();
 
+const app = express();
+
+
+// Middleware
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173", // frontend URL
+  origin: "http://localhost:5173",
   credentials: true
 }));
 
+
+
+// Port
 const PORT = process.env.PORT || 5000;
 
-// Start server
+// Start Server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });

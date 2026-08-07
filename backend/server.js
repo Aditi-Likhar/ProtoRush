6a987cb9c1f0b1504ea11f15f29a7ea59a0142fc
Aditi-Likhar <aditilikhar1646@gmail.com>
2026-08-07 08:20:28 +0530

    Backend setup completed

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
+import connectDB from "./config/db.js";
 
-
-// Load env variables
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
-
-// Middleware
 app.use(express.json());
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}));
+
+app.get("/", (req, res) => {
+  res.send("Server Running");
+});
 
 const PORT = process.env.PORT || 5000;
 
-
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
